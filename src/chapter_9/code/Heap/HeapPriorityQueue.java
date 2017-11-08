14e7659fef332ba71dab45727a8440919bd03abf
gabrielhfgomes <gabrielhfgomes@hotmail.com>
2017-11-08 17:23:24 -0200

    C937

--- src/chapter_9/code/Heap/HeapPriorityQueue.java
+++ src/chapter_9/code/Heap/HeapPriorityQueue.java
@@ -153,7 +153,7 @@ public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
         }
     }
 
-
+    //C937
     public void combine(HeapPriorityQueue<K,V> t1, HeapPriorityQueue<K,V> t2) {
         while(t1.size() != 0) {
             Entry<K,V> temp = t1.removeMin();

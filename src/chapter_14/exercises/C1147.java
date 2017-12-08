5871b1aed98c9818e5bee20bedfcb08c1094194a
Jasiel Josias Macagnan <jlmacagnan@gmail.com>
2017-12-08 15:18:36 -0200

    R-14.6, R-14.7

---
+++ src/chapter_14/exercises/C1147.java
@@ -0,0 +1,8 @@
+package chapter_14.exercises;
+
+/*C-14.37 Give a Java implementation of the removeEdge(e) method for our adjacency map
+implementation of Section 14.2.5, making sure your implementation works for
+both directed and undirected graphs. Your method should run in O(1) time.*/
+
+public class C1147 {
+}

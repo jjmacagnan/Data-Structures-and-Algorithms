843056867172d9feb5b9e43b2ed36d846be5d3d0
gabrielhfgomes <gabrielhfgomes@hotmail.com>
2017-12-11 13:08:12 -0200

    R127

--- src/chapter_12/exercises/R127.java
+++ src/chapter_12/exercises/R127.java
@@ -13,20 +13,36 @@ public class R127 {
     public static <K> void merge(K[] S1, K[] S2, K[] S, Comparator<K> comp) {
         int i = 0, j = 0;
 
-        while (i + j < S.length)
-            if (j == S2.length || (i < S1.length && comp.compare(S1[i], S2[j]) < 0 ))
-                S[i + j] = S1[i++];                             // copy ith element of S1 and increment i
+        while (S1.length != 0 && S2.length != 0) {
+            if (j == S2.length || (i < S1.length && comp.compare(S1[i], S2[j]) < 0))
+                if(i != 0) {
+                    if (S[(i + j) - 1] != S1[i]) {
+                        S[i + j] = S1[i];
+                    }
+                    i++;
+                } else {
+                    S[i + j] = S1[i++];
+                }
             else
-                S[i + j] = S2[j++];                             // copy jth element of S2 and increment j
-
+                if(i != 0 ) {
+                    if (S[(i + j) - 1] != S[j]) {
+                        S[i + j] = S2[j];
+                    }
+                    j++;
+                } else {
+                    S[i + j] = S2[j++];
+                }
+                // copy jth element of S2 and increment j
+        }
     }
 
 
     public static void main(String[] args) {
         Comparator comp = new DefaultComparator();
-        Object[] array1 = {0, 1, 2, 3, 4};
-        Object[] array2 = {4, 5, 6, 7, 8};
-        Object[] arrayResult = {};
+        Integer[] array1 = {0, 1, 2, 3, 4};
+        Integer[] array2 = {4, 5, 6, 7, 8};
+        Integer[] arrayResult = new Integer[array1.length+array2.length];
+
 
         R127.merge(array1, array2,arrayResult, comp);
 

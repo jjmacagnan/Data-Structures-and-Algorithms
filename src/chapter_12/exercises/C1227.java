8a876f6279c881369195d45c3570b7e8fa1f50c2
jjmacagnan <jlmacagnan@gmail.com>
2017-12-18 14:43:15 -0200

    R-12.27

--- src/chapter_12/exercises/C1227.java
+++ src/chapter_12/exercises/C1227.java
@@ -2,6 +2,10 @@ package chapter_12.exercises;
 
 import chapter_7.code.iterators.LinkedPositionalList;
 
+/*C-12.27 Augment the PositionalList class (see Section 7.3) to support a method named
+sort that sorts the elements of a list by relinking existing nodes; you are not to
+create any new nodes. You may use your choice of sorting algorithm.*/
+
 public class C1227 {
 
     public static void main(String[] args) {

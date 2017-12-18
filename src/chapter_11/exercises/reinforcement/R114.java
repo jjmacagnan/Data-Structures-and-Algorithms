8a4311794301e3eeec960d3196bb19c9944035a2
jjmacagnan <jlmacagnan@gmail.com>
2017-12-18 10:17:58 -0200

    R-11.4

--- src/chapter_11/exercises/reinforcement/R114.java
+++ src/chapter_11/exercises/reinforcement/R114.java
@@ -2,18 +2,22 @@ package chapter_11.exercises.reinforcement;
 
 import chapter_11.code.TreeMap;
 
+/*R-11.4 Dr. Amongus claims that the order in which a fixed set of entries is inserted into
+a binary search tree does not matter—the same tree results every time. Give a
+small example that proves he is wrong.*/
+
 public class R114 {
 
     public static void main(String[] args) {
         TreeMap treeMap = new TreeMap();
 
+        treeMap.put(48, null);
         treeMap.put(30, null);
         treeMap.put(40, null);
+        treeMap.put(13, null);
         treeMap.put(24, null);
         treeMap.put(58, null);
-        treeMap.put(48, null);
         treeMap.put(26, null);
-        treeMap.put(13, null);
 
         treeMap.toString(treeMap.root());
     }
